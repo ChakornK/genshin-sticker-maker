46d272aadf95e980b3d828e2a2bd363acf3869c2
ChakornK <chakornk2007@gmail.com>
2025-11-28 23:44:43 -0800

    feat: add sticker preview

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 import { Slider } from "../../components/Slider";
 
@@ -6,6 +6,10 @@ import data from "../../data.yml";
 
 import "./style.css";
 import { Button } from "../../components/Button";
+import { Application, Assets, Sprite, Text } from "pixi.js";
+
+const RESOURCE_BASE_URL =
+  "https://cdn.jsdelivr.net/gh/ChakornK/genshin-sticker-maker/assets";
 
 export function Home() {
   return (
@@ -22,7 +26,8 @@ function Editor() {
   const [x, setX] = useState(50);
   const [y, setY] = useState(0);
   const [rotation, setRotation] = useState(0);
-  const [fontSize, setFontSize] = useState(12);
+  const [fontSize, setFontSize] = useState(24);
+  const [textContent, setTextContent] = useState("Hello!");
 
   return (
     <div class={"flex flex-col gap-8"}>
@@ -30,7 +35,15 @@ function Editor() {
         class={"flex flex-col items-center gap-8 sm:flex-row sm:items-start"}
       >
         <div class={"grid grid-cols-[2fr_2em] grid-rows-[2fr_2em]"}>
-          <Preview />
+          <Preview
+            characterName="Klee"
+            characterNum="1"
+            textContent={textContent}
+            textSize={fontSize}
+            textRotation={rotation}
+            textX={x}
+            textY={y}
+          />
           <Slider
             vertical
             value={y}
@@ -56,13 +69,19 @@ function Editor() {
           <p class={"mt-4"}>Font size</p>
           <Slider
             value={fontSize}
-            min={1}
-            max={48}
+            min={12}
+            max={96}
             onChange={(v) => setFontSize(v)}
           />
 
           <p class={"mt-4"}>Text</p>
-          <textarea class={"mt-2 rounded-lg bg-black/30 p-2"}></textarea>
+          <textarea
+            class={"mt-2 rounded-lg bg-black/30 p-2"}
+            value={textContent}
+            onInput={(e) =>
+              setTextContent((e.target as HTMLTextAreaElement).value)
+            }
+          />
         </div>
       </div>
       <div
@@ -81,6 +100,107 @@ function Editor() {
   );
 }
 
-function Preview() {
-  return <div class={"bg-darker h-64 w-64 rounded-xl"}></div>;
+function Preview({
+  characterName,
+  characterNum,
+  textContent,
+  textSize,
+  textRotation,
+  textX,
+  textY,
+}: {
+  characterName: string;
+  characterNum: string;
+  textContent: string;
+  textSize: number;
+  textRotation: number;
+  textX: number;
+  textY: number;
+}) {
+  const [ready, setReady] = useState(false);
+  const [app] = useState(new Application());
+  const appContainer = useRef<HTMLDivElement>(null);
+
+  const [sprite, setSprite] = useState<Sprite>(null);
+  const [text, setText] = useState<Text>(null);
+
+  useEffect(() => {
+    (async () => {
+      if (document.getElementById("preview-canvas")) return;
+
+      await app.init({
+        resizeTo: appContainer.current!,
+        antialias: true,
+        webgl: {
+          antialias: true,
+        },
+        backgroundColor: 0x000000,
+        backgroundAlpha: 0,
+        resolution: 2,
+      });
+      app.canvas.id = "preview-canvas";
+      appContainer.current!.appendChild(app.canvas);
+
+      const s = new Sprite({
+        anchor: { x: 0.5, y: 0.5 },
+        width: app.screen.width,
+        height: app.screen.height,
+        x: app.screen.width / 2,
+        y: app.screen.height / 2,
+      });
+      app.stage.addChild(s);
+
+      await Assets.load({
+        src: `${RESOURCE_BASE_URL}/YurukaStd.woff2`,
+        data: { family: "Yuruka" },
+      });
+      const t = new Text({
+        anchor: { x: 0.5, y: 0.5 },
+        angle: textRotation,
+        style: {
+          fontFamily: "Yuruka",
+          fontSize: textSize,
+          align: "center",
+          stroke: {
+            color: 0xffffff,
+            width: 8,
+          },
+        },
+        text: textContent,
+        x: (textX / 100) * app.screen.width,
+        y: (textY / 100) * app.screen.height,
+      });
+      app.stage.addChild(t);
+
+      setSprite(s);
+      setText(t);
+      setReady(true);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (!ready || !sprite) return;
+      const tex = await Assets.load(
+        `${RESOURCE_BASE_URL}/${characterName.toLowerCase()}/${characterNum}.png`
+      );
+      sprite.texture = tex;
+    })();
+  }, [ready, sprite, characterName, characterNum]);
+
+  useEffect(() => {
+    if (!ready || !text) return;
+    text.text = textContent;
+    text.style.fontSize = textSize;
+    text.angle = textRotation;
+    text.x = (textX / 100) * app.screen.width;
+    text.y = (textY / 100) * app.screen.height;
+  }, [ready, text, textContent, textSize, textRotation, textX, textY]);
+
+  return (
+    <div
+      ref={appContainer}
+      class={"bg-darker *:w-full *:h-full h-64 w-64 rounded-xl"}
+    ></div>
+  );
 }
