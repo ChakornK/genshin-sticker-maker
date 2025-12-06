a7104f6f2bdb5f74b3e5ad6a722224a8b30fea8b
ChakornK <chakornk2007@gmail.com>
2025-12-06 15:57:08 -0800

    feat: dynamically generate meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,34 @@ export default defineConfig({
         previewMiddlewareFallback: "/404",
       },
     }),
+    {
+      name: "meta-tags",
+      transformIndexHtml(html) {
+        return ((
+          html: string,
+          {
+            title,
+            description,
+            image,
+          }: { title: string; description: string; image: string }
+        ) => {
+          const metaHtml = `
+		        <title>${title}</title>
+		        <meta property="og:description" content="${description}">
+		        <meta property="og:title" content="${title}">
+		        <meta property="og:image" content="${image}">
+		        <meta name="description" content="${description}">
+		        <meta name="twitter:title" content="${title}">
+		        <meta name="twitter:description" content="${description}">
+		        <meta name="twitter:card" content="summary">
+		        <meta name="twitter:image" content="${image}">`;
+          return html.replace(/(?<=<head>)/, metaHtml);
+        })(html, {
+          title: "Genshin Sticker Maker",
+          description: "Create stickers with Genshin characters",
+          image: `${process.env.VITE_RESOURCE_BASE_URL || ""}/favicon.png`,
+        });
+      },
+    },
   ],
 });
